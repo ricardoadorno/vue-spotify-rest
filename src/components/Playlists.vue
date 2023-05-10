62e44da1d766dc010e6cede7a1ab18b6e2bccf4f
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-05-10 11:20:30 -0300

    ADD: spotify credentials

--- src/components/Playlists.vue
+++ src/components/Playlists.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// fetch playlists from spotify
+async function fetchProfile(): Promise<any> {
+  const result = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_APP_SPOTIFY_TOKEN}`,
+    },
+  });
+
+  return await result.json();
+}
+
+fetchProfile().then((data) => console.log(data));
+</script>
 
 <template>
   <main>
